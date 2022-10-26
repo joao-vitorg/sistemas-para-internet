bfad8a9176204f1634cea834ed7739215010c368
joao_vitorg <jovitor737@gmail.com>
2022-10-26 17:49:31 -0300

    [Algo] Estrutura dos arquivos HashSet alterada

--- 2-semestre/algoritimos/linguagem/hash/set/QuantidadeEmails.java
+++ 2-semestre/algoritimos/linguagem/hash/set/QuantidadeEmails.java
@@ -1,3 +1,5 @@
+package set;
+
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
--- 2-semestre/algoritimos/linguagem/hash/set/QuantidadePalavras.java
+++ 2-semestre/algoritimos/linguagem/hash/set/QuantidadePalavras.java
@@ -1,3 +1,5 @@
+package set;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
