94c01b77fa2191a9494be60d07e299398e947364
joao_vitorg <jovitor737@gmail.com>
2022-11-05 20:36:36 -0300

    [MVC] Bando de dados alterado para MySQL

---
+++ 2-semestre/projeto-mvc/src/main/resources/import.sql
@@ -0,0 +1,43 @@
+-- Database professor com nome
+create database professor;
+use professor;
+create table professor
+(
+    id   int          not null primary key auto_increment,
+    nome varchar(100) not null
+);
+
+-- Database contato com nome telefone, endereço e tipo com descrição
+create database contato;
+use contato;
+create table tipo
+(
+    id        int          not null primary key auto_increment,
+    descricao varchar(100) not null
+);
+create table pessoa
+(
+    id       int          not null primary key auto_increment,
+    nome     varchar(100) not null,
+    telefone varchar(20)  not null,
+    endereco varchar(100) not null,
+    id_tipo  int          not null references tipo (id)
+);
+
+-- Database prédio
+create database predio;
+use predio;
+create table proprietario
+(
+    id       int         not null primary key auto_increment,
+    nome     varchar(50) not null,
+    telefone varchar(20) not null
+);
+create table apartamento
+(
+    id              int         not null primary key auto_increment,
+    numero          int         not null unique,
+    qtd_quartos     int         not null,
+    tipo_ocupacao   varchar(20) not null,
+    id_proprietario int         not null references proprietario (id)
+);
